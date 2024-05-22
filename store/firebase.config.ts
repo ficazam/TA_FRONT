8248289dea065abbaa917c1b6d711c1b6153e610
Felipe Icaza <112637352+wl-felipe@users.noreply.github.com>
2024-05-22 15:44:41 -0500

    implements login + logout

--- store/firebase.config.ts
+++ store/firebase.config.ts
@@ -2,6 +2,7 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+const auth = getAuth(app)
 //const analytics = getAnalytics(app);
 
 export { app, auth };
